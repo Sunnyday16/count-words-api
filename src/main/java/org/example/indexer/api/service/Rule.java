7bf73b30c025c9d10813c01302904ebee7606877
Sunnyday16 <sunny.k.kr@gmail.com>
2025-08-15 23:20:57 +1000

    Delete src directory

--- src/main/java/org/example/indexer/api/service/Rule.java
+++
@@ -1,8 +0,0 @@
-package org.example.indexer.api.service;
-
-import java.util.stream.Stream;
-
-public interface Rule<R> {
-  String name();                       // JSON key
-  R apply(Stream<String> words);       // compute result from a fresh Stream
-}
